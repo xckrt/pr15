e6a87efdb23da077b8d161817d94712d211002a3
xckrt <61594501+xckrt@users.noreply.github.com>
2024-05-28 11:06:39 +0500

    ada

--- app/src/main/java/com/example/pr15/first.kt
+++ app/src/main/java/com/example/pr15/first.kt
@@ -1,6 +1,8 @@
 package com.example.pr15
 
+import android.content.Intent
 import android.health.connect.datatypes.units.Length
+import android.net.Uri
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
@@ -8,9 +10,12 @@ import android.widget.TextView
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.graphics.vector.PathNode
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.system.exitProcess
 
 class First : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -22,6 +27,8 @@ class First : AppCompatActivity() {
     )
 
     private var currentQuestionIndex = 0
+    private var correctAnswersCount = 0
+    private var incorrectAnswersCount = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -56,10 +63,12 @@ class First : AppCompatActivity() {
     private fun checkAnswer(userAnswer: Boolean) {
         val currentQuestion = questionsAndAnswers[currentQuestionIndex]
         if (userAnswer == currentQuestion.answer) {
+            correctAnswersCount++
             val toast = Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_SHORT)
             toast.setGravity(Gravity.TOP,0,0)
             toast.show()
         } else {
+            incorrectAnswersCount++
             val toast = Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_SHORT)
             toast.setGravity(Gravity.TOP,0,0)
             toast.show()
@@ -71,8 +80,43 @@ class First : AppCompatActivity() {
         }
         else
         {
-            Toast.makeText(this,getString(R.string.end),Toast.LENGTH_SHORT).show()
-            application.onTerminate()
+            showResultsDialog()
         }
     }
+
+    fun nextquestion(view: View) {
+        if (currentQuestionIndex < questionsAndAnswers.size - 1) {
+            currentQuestionIndex++
+            setQuestion()
+        } else {
+            Toast.makeText(this, getString(R.string.lastquest), Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun backquestion(view: View) {
+        if (currentQuestionIndex > 0) {
+            currentQuestionIndex--
+            setQuestion()
+        } else {
+            Toast.makeText(this, getString(R.string.backerror), Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun showResultsDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.finish))
+        builder.setMessage("${getString(R.string.correctans)} $correctAnswersCount\n${getString(R.string.incorrectans)} $incorrectAnswersCount")
+        builder.setPositiveButton(getString(R.string.OK)) { dialog, _ ->
+            dialog.dismiss()
+            application.onTerminate()
+
+
+        }
+        builder.show()
+    }
+
+    fun endquest(view: View) {
+        val toast = Toast.makeText(this, getString(R.string.testend),Toast.LENGTH_SHORT)
+        toast.show()
+        System.exit(0)
+    }
+
 }
